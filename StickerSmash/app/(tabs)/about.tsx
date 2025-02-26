9f8bd987be058d1344881cd625d8df879636c479
OrtigosaLaura <lauraortigosa378@gmail.com>
2025-02-26 10:51:00 -0300

    nnnnnnnnnnnnnnnnnnnnn

---
+++ StickerSmash/app/(tabs)/about.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style= {styles.text}>Sobre</Text>
+    </View>
+  )
+}
+ const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color:'#fff',
+  }
+ })
