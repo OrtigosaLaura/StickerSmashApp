5cb543847700b2149b1bf245bec199e4eaeb4878
OrtigosaLaura <lauraortigosa378@gmail.com>
2025-04-02 10:39:19 -0300

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ StickerSmash/components/EmojiList.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, FlatList, Platform, Pressable } from 'react-native'
+import { useState } from 'react'
+import { Image, type ImageSource } from 'expo-image'
+
+type Props ={
+    onSelect: (image: ImageSource) => void;
+    onCloseModel: () => void;
+}
+
+
+
+export default function EmojiList() {
+  return (
+    <View>
+      <Text>EmojiList</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
